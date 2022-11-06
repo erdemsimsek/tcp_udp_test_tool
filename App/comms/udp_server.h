60dca284d50d80dd042abca366311ecebb71d739
erdemsimsek <erdem@erdemsimsek.info>
2022-11-06 14:22:01 +0000

    Finalising udp server

--- App/comms/udp_server.h
+++ App/comms/udp_server.h
@@ -1,15 +1,15 @@
 #pragma once
 
 #include <string>
+#include <thread>
 #include <boost/asio.hpp>
 #include <boost/bind.hpp>
 
-
-
+namespace comms{
 class UdpServer{
 public:
 
-using rxCbFxn = void(*)(char * msg, size_t msgLen);
+    using rxCbFxn = void(*)(const char * msg, size_t msgLen);
 
     UdpServer(rxCbFxn rxCallbackFunc, size_t portNumber) : 
         rxCballbackFunc{rxCallbackFunc}, 
@@ -18,17 +18,25 @@ using rxCbFxn = void(*)(char * msg, size_t msgLen);
 
     }
 
-    void startListenning()
+    UdpServer(rxCbFxn rxCallbackFunc, unsigned long ipAddr, size_t portNumber) : 
+        rxCballbackFunc{rxCallbackFunc}, 
+        socket{ioService, boost::asio::ip::udp::endpoint(boost::asio::ip::address_v4(ipAddr), portNumber)}
     {
-        socket.async_receive_from(
-            boost::asio::buffer(buffer), remoteEndpoint,
-            boost::bind(&UdpServer::handleReceivedData, this,
-                boost::asio::placeholders::error,
-                boost::asio::placeholders::bytes_transferred));
+
     }
 
-    void execute()
+    void sendMessage(std::string message)
     {
+        auto msgTpSend = std::make_shared<std::string>(message);
+        socket.async_send_to(boost::asio::buffer(*msgTpSend), remoteEndpoint,
+                boost::bind(&UdpServer::handleSend, this, msgTpSend,
+                    boost::asio::placeholders::error,
+                    boost::asio::placeholders::bytes_transferred));
+    }
+
+    void runComms()
+    {
+        startListenning();
         ioService.run();
     }
 
@@ -43,24 +51,35 @@ private:
     {
         if (!error || error == boost::asio::error::message_size) {
             std::string receivedMessage{buffer.begin()};
-            std::cout << receivedMessage << "\n";
+            std::cout << receivedMessage;
 
-            auto message = std::make_shared<std::string>("Hello, World\n");
+            if(nullptr != rxCballbackFunc)
+            {
+                rxCballbackFunc(buffer.begin(), bytesTransferred);
+            }
 
-            socket.async_send_to(boost::asio::buffer(*message), remoteEndpoint,
-                boost::bind(&UdpServer::handleSend, this, message,
-                    boost::asio::placeholders::error,
-                    boost::asio::placeholders::bytes_transferred));
+            startListenning();
         }
     }
 
     void handleSend(std::shared_ptr<std::string> message,
                     const boost::system::error_code& ec,
                     std::size_t bytes_transferred) {
-        startListenning();
+    }
+
+    void startListenning()
+    {
+        socket.async_receive_from(
+            boost::asio::buffer(buffer), remoteEndpoint,
+            boost::bind(&UdpServer::handleReceivedData, this,
+                boost::asio::placeholders::error,
+                boost::asio::placeholders::bytes_transferred));
     }
 
 
 };
+
+}
+
 
 
